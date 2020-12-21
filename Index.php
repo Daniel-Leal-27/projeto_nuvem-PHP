3de43193cbcc6abd4a6938534c85f46680abbe78
Daniel-Leal-27 <70525319+Daniel-Leal-27@users.noreply.github.com>
2020-12-21 09:22:49 -0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -3,15 +3,12 @@
     <title>Connect</title>
 </head>
 <body>
-    <h1>Lista de produtos registrados no banco de dados!</h1>
-</body>
-<body>
 <?php
 
 $servername = "localhost";
 $username = "root";
 $password = "password";
-$database = "Allbikes";
+$database = "allbikes01";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
@@ -26,8 +23,33 @@ $result = $conn->query($sql);
 while($row = $result->fetch_assoc()){
     echo "<p>  ID: ".$row['id']." Nome do produto: ".$row['nome']." Descrição do produto: ".$row['descricao']." Preço do produto: R$".$row['preco']." Imagem do produto: ".$row['imagem'];
 }
+
+/*$servername = "localhost";
+$username = "root";
+$password = "password";
+$database = "faculdade2";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn){
+    die("A conexão falhou".mysqli_connect_error());
+
+}
+
+$sql = "select id_aluno, nome, idade, logradouro, num, bairro, cid, est from alunos";
+$result = $conn->query($sql);
+
+while($row = $result->fetch_assoc()){
+    echo "<p>  ID do aluno: ".$row['id_aluno']." Nome do aluno: ".$row['nome']." Idade: ".$row['idade']." Logradouro: ".$row['logradouro']." número: ".$row['num']." bairro: ".$row['bairro']." cidade: ".$row['cid']." estado: ".$row['est'];
+}
+$sql = "select id, nome, descricao, preco, imagem from Produtos";
+$result = $conn->query($sql);
+
+while($row = $result->fetch_assoc()){
+    echo "<p>  ID: ".$row['id']." Nome do produto: ".$row['nome']." Descrição do produto: ".$row['descricao']." Preço do produto: R$".$row['preco']." Imagem do produto: ".$row['imagem'];
+}
+*/
+
 ?>
-<br><br>
-<a href="Conect2.php">Faça seu pedido clicando aqui</a>
 </body>
 </html>
